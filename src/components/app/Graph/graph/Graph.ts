7f9969a1fd1ac0764178e92e26eb4b68e6be86b0
Ryan Weiss <rw3iss@gmail.com>
2025-01-21 16:36:11 -0500

    (no commit message)

--- src/components/app/Graph/graph/Graph.ts
+++ src/components/app/Graph/graph/Graph.ts
@@ -115,7 +115,7 @@ export default class Graph extends CanvasDrawingContext {
         let yStart = 0 + this.height - this.axesPadding - this.axesLineWidth;
 
         let drawData = (i, point, dataWidth, nextPt, color) => {
-            console.log(`drawData`, i, point, dataWidth, color);
+            //console.log(`drawData`, i, point, dataWidth, color);
             let xPos = i * dataWidth;
             // yh = (graph height) * (this value's percent over total y) - (paddings)
             let yHeight = this.height * ((point.y - this.yMin) / (this.yMax - this.yMin)) - (this.axesPadding * 2) - this.axesLineWidth / 2;
